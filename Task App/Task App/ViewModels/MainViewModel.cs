795634f81e41774ec37875486764f0b5868cb100
DESKTOP-SO9CMK4\Conor <conor_a_murphy@outlook.com>
2025-08-12 17:30:16 +0100

    Add Base and Main ViewModels

---
+++ Task App/Task App/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Task_App.ViewModels
+{
+    public partial class MainViewModel : BaseViewModel
+    {
+
+        public ICommand TaskCommand { get; }
+
+        [ObservableProperty]
+        ObservableCollection<string> task = new();
+
+        public MainViewModel()
+        {
+            TaskCommand = new Command(TaskAdded);
+        }
+
+        public void TaskAdded() 
+        {
+            Task.Add("Task Added");
+        }
+    }
+}
